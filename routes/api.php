fc4ec5fb7693e2c9dc7caced620febabbb6467da
Cleverson Sucalit <cleversonsucalit@gmail.com>
2024-07-19 10:16:27 +0800

    test comment

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\MovieController;
 use App\Http\Controllers\DirectorController;
 use App\Http\Controllers\ActorController;
 use App\Http\Controllers\AuthController;
-
+// test
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/movies', [MovieController::class, 'index']);
     Route::get('/movies/{id}', [MovieController::class, 'show']);
