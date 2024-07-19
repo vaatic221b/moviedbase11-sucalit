35e320404ff6ccf8204e65be5e6eaa76f423a0e0
Cleverson Sucalit <cleversonsucalit@gmail.com>
2024-07-19 10:54:22 +0800

    route/api.php more comments

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,14 @@ use App\Http\Controllers\MovieController;
 use App\Http\Controllers\DirectorController;
 use App\Http\Controllers\ActorController;
 use App\Http\Controllers\AuthController;
-// test
+// BCL4 push 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/movies', [MovieController::class, 'index']);
-    Route::get('/movies/{id}', [MovieController::class, 'show']);
-    Route::get('/directors/{id}', [DirectorController::class, 'show']);
-    Route::get('/actors/{id}', [ActorController::class, 'show']);
-    Route::get('/movies/genres', [MovieController::class, 'moviesByGenre']); //needs fixing
-    Route::get('/movies/ratings', [MovieController::class, 'moviesByRating']); //needs fixing
+    Route::get('/movies/{id}', [MovieController::class, 'show']); //http://127.0.0.1:8000/api/movies/901 example movie
+    Route::get('/directors/{id}', [DirectorController::class, 'show']); //http://127.0.0.1:8000/api/directors/201 example director
+    Route::get('/actors/{id}', [ActorController::class, 'show']); //http://127.0.0.1:8000/api/actors/101 example actor
+    Route::get('/movies/genres', [MovieController::class, 'moviesByGenre']); //NEEDS FIXING
+    Route::get('/movies/ratings', [MovieController::class, 'moviesByRating']); //NEEDS FIXING
 });
 
 
